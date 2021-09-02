799d7533ecc848ef100e82566b50f64f15b3f2ee
Jason-Donmoyer <j.donmoyer523@gmail.com>
2021-09-02 06:06:31 -0400

    HTML and JS: -HTML: --removed hard coded values -JS: --countdown functionality

--- js/main.js
+++ js/main.js
@@ -1,19 +1,70 @@
 // VARIABLES
 
 let countdownDateHeading = document.getElementById("countdown-date-heading");
+let UIDays = document.getElementById("days");
+let UIHours = document.getElementById("hours");
+let UIMinutes = document.getElementById("minutes");
+let UISeconds = document.getElementById("seconds");
 
-const today = new Date();
+
+// Date Functionality
+const today = addMonths(new Date(), 4);
 
 const dayOfMonth = today.getDate();
-let year = today.getFullYear();
+const month = getNewMonth(today.getMonth());
+const year = today.getFullYear();
 
-// Get new month for text purposes
-let newMonth;
-if (today.getMonth() + 4 > 11) {
-  newMonth = (today.getMonth() + 4) - 12;
-  year++;
+const fullDay = dayOfMonth + ' ' + month + ' ' + year;
+console.log(fullDay);
+
+
+// Countdown Functionality
+
+// Target Date to Count Down To
+const targetDate = today.getTime();
+
+// Set Interval Function to Count Down From today
+let timer = setInterval(function() {
+  
+  // Current date and time
+  const now = new Date().getTime();
+
+  // Difference in times
+  let dif = targetDate - now;
+
+  // Calculate days, hours, minutes, seconds
+  let days = Math.floor(dif / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dif % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dif % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dif % (1000 * 60)) / 1000);
+
+  UIDays.innerHTML = days;
+  UIHours.innerHTML = hours;
+  UIMinutes.innerHTML = minutes;
+  UISeconds.innerHTML = seconds;
+
+}, 1000);
+
+// Dynamic Values for UI
+
+// Heading Text
+countdownDateHeading.innerHTML = fullDay;
+
+
+// FUNCTIONS
+
+// Function to add months to date
+// https://stackoverflow.com/questions/2706125/javascript-function-to-add-x-months-to-a-date
+function addMonths(date, months) {
+  var d = date.getDate();
+  date.setMonth(date.getMonth() + +months);
+  if (date.getDate() != d) {
+    date.setDate(0);
+  }
+  return date;
 }
 
+// Function to get text for month 
 function getNewMonth(today) {
   let month;
   // Switch Statement for months
@@ -57,10 +108,3 @@ function getNewMonth(today) {
   }
   return month;
 }
-
-const month = getNewMonth(newMonth);
-
-const fullDay = dayOfMonth + ' ' + month + ' ' + year;
-console.log(fullDay);
-
-countdownDateHeading.innerHTML = fullDay;
